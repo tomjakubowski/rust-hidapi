de0dd8e81141f22a792627229d26791af91b4355
Tom Jakubowski <tom@crystae.net>
2014-06-07 01:39:16 -0700

    Document all of the things

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@ RUSTBINDIR = bin
 blink1_CRATE_DEPS += hidapi
 x360_CRATE_DEPS += hidapi
 
+blink1_DONT_DOC = 1
+x360_DONT_DOC = 1
+
 include rust-mk/rust.mk
--- src/hidapi/lib.rs
+++ src/hidapi/lib.rs
@@ -2,6 +2,50 @@
 #![crate_type = "lib"]
 #![license = "MIT"]
 
+/*!
+
+A Rust library for communicating with USB Human Interface Devices
+using [`hidapi`](https://github.com/signal11/hidapi).
+
+For example, you can send and receive feature reports with a
+hypothetical USB cut of beef:
+
+```no_run
+use hidapi::HidDevice;
+
+static MEAT_VENDOR_NUM: u16 = 0xdead;
+static MEAT_PRODUCT_NUM: u16 = 0xbeef;
+
+let steak = HidDevice::open(MEAT_VENDOR_NUM, MEAT_PRODUCT_NUM)
+              .expect("where's the beef?");
+steak.send_feature_report(bytes!(0x0, "MOO"));
+
+let mut doneness = [0x01, 0x00, 0x00, 0x00, 0x00];
+steak.get_feature_report(doneness);
+assert_eq!(doneness.as_slice(), bytes!(1, "RARE"));
+```
+
+You can also enumerate the various cuts of beef attached to your
+computer to find one that's also a golf club:
+
+```no_run
+use hidapi::{HidDevice, HidDeviceInfo};
+
+static MEAT_VENDOR_NUM: u16 = 0xdead;
+static MEAT_PRODUCT_NUM: u16 = 0xbeef;
+static SPORTS_USAGE_PAGE: u16 = 0x04;
+static GOLF_CLUB_USAGE: u16 = 0x02;
+
+let steaks = hidapi::enumerate(MEAT_VENDOR_NUM, MEAT_PRODUCT_NUM);
+let golf_steak: &HidDeviceInfo = steaks.iter().find(|&inf| {
+    inf.usage_page == SPORTS_USAGE_PAGE && inf.usage == GOLF_CLUB_USAGE
+}).expect("no golf steak found");
+
+let device = HidDevice::open_from_info(golf_steak).unwrap();
+device.send_feature_report(bytes!(0x0, "FORE"));
+```
+ */
+
 extern crate libc;
 extern crate sync;
 
@@ -55,11 +99,15 @@ mod ffi {
     }
 }
 
+/// Opaque structure representing a connected HID device.
 pub struct HidDevice {
     dev: *mut ffi::HidDevice
 }
 
 impl HidDevice {
+    /// Attempts to open a HID device using a `vendor_id` and
+    /// `product_id`. Returns `None` if a device was not found.
+    // FIXME: allow open with serial number
     pub fn open(vendor_id: libc::c_ushort, product_id: libc::c_ushort) -> Option<HidDevice> {
         use std::ptr::RawPtr;
 
@@ -73,10 +121,13 @@ impl HidDevice {
         }
     }
 
+    /// Attempts to open a HID device using the structure returned by
+    /// [`hidapi::enumerate`](fn.enumerate.html).
     pub fn open_from_info(info: &HidDeviceInfo) -> Option<HidDevice> {
         HidDevice::open_path(info.path.as_slice())
     }
 
+    /// Attempts to open a HID device by its path name.
     pub fn open_path(path: &str) -> Option<HidDevice> {
         unsafe { init(); }
 
@@ -91,6 +142,8 @@ impl HidDevice {
         }
     }
 
+    /// Sends a feature report to the device. The first byte of `buf`
+    /// should be the report ID.
     // FIXME: figure out how to deal with errors
     // FIXME: should these all be &mut self?
     pub fn send_feature_report(&self, buf: &[u8]) {
@@ -99,6 +152,9 @@ impl HidDevice {
         }
     }
 
+    /// Reads a feature report from the device into `buf`. The first
+    /// byte of `buf` should be set to the report ID to be read before
+    /// calling this method.
     pub fn get_feature_report(&self, buf: &mut [libc::c_uchar]) {
         unsafe {
             let size = ffi::hid_get_feature_report(self.dev, &mut buf[0], buf.len() as size_t);
@@ -109,6 +165,7 @@ impl HidDevice {
         };
     }
 
+    /// Reads an input report from the device into `buf`.
     // FIXME: add support for hid_read_timeout
     pub fn read(&self, buf: &mut [libc::c_uchar]) {
         unsafe {
@@ -119,6 +176,7 @@ impl HidDevice {
         }
     }
 
+    /// Writes an output report to the device.
     pub fn write(&self, buf: &[libc::c_uchar]) {
         unsafe {
             ffi::hid_write(self.dev, &buf[0], buf.len() as size_t);
@@ -139,9 +197,12 @@ pub struct HidDeviceInfo {
     pub path: String,
     pub vendor_id: libc::c_ushort,
     pub product_id: libc::c_ushort,
+    #[doc(hidden)]
     pub serial_number: String,
     pub release_number: libc::c_ushort,
+    #[doc(hidden)]
     pub manufacturer_string: String,
+    #[doc(hidden)]
     pub product_string: String,
     pub usage_page: libc::c_ushort,
     pub usage: libc::c_ushort,
@@ -167,6 +228,10 @@ impl HidDeviceInfo {
     }
 }
 
+/// Returns a vector of the HID devices that match `vendor_id` and
+/// `product_id`. If `vendor_id` is set to 0, any vendor matches. If
+/// `product_id` is set to 0, any product matches. If both `vendor_id`
+/// and `product_id` are set to 0, all HID devices are returned.
 pub fn enumerate(vendor_id: libc::c_ushort, product_id: libc::c_ushort) -> Vec<HidDeviceInfo> {
     unsafe {
         init();
