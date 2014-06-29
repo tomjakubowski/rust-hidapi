a534bae3fed13b65df93ac35eb18d9707c3b5b94
Tom Jakubowski <tom@crystae.net>
2014-06-29 03:43:29 -0700

    sigh

--- .travis.after.sh
+++ .travis.after.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]];
 then
     curl http://www.rust-ci.org/artifacts/put?t=$RUSTCI_TOKEN | sh
