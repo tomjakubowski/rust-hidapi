4ce80d92375e6386855f1d28b34b343f579d566a
Tom Jakubowski <tom@crystae.net>
2014-06-29 03:50:55 -0700

    Fix Rust-CI token

--- .travis.after.sh
+++ .travis.after.sh
@@ -3,7 +3,10 @@ set -ex
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]];
 then
+    echo "Uploading..."
+    set +x
     curl http://www.rust-ci.org/artifacts/put?t=$RUSTCI_TOKEN | sh
+    set -x
 fi
 
 set +x
